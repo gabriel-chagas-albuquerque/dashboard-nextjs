210615224be4afd6fa5e6669cce6577ca1d73942
Gabriel Albuquerque <gabrieldafortaleza0100@gmail.com>
2024-07-16 20:09:33 -0300

    Seeding database adjusts

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -103,8 +103,10 @@ async function seedRevenue() {
 
 export async function GET() {
   return Response.json({
-    message:
-      "Uncomment this file and remove this line. You can delete this file when you are finished.",
+    users,
+    customers,
+    revenue,
+    invoices
   });
   try {
     await client.sql`BEGIN`;
